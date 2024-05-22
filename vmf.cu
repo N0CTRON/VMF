7808fd842d128f9d552ccc07ca88afdf52590378
XeTute <154016609+N0CTRON@users.noreply.github.com>
2024-05-22 20:34:34 +0200

    Ported new functions which can be parali. to CUDA

--- vmf.cu
+++ vmf.cu
@@ -2,16 +2,23 @@
 #include <device_launch_parameters.h>
 #include <cuda/std/cmath>
 
-typedef unsigned long int cuSize; //<-- The GTX1060 3GB doesn't support size_t
-
 namespace VMF_CUDA
 {
+    typedef unsigned long int cuSize; //<-- The GTX1060 3GB doesn't support size_t
     // ReLU
     template <typename vmfDevType> //<-- vmfDevType = VMF[Various Math Functions] Devoloper Type
     __global__ void ReLU(vmfDevType* x, cuSize arraySize)
     {
         cuSize threadIndexX = blockIdx.x * blockDim.x + threadIdx.x;
         if (threadIndexX < arraySize) x[threadIndexX] = x[threadIndexX] > vmfDevType(0) ? x[threadIndexX] : vmfDevType(0);
+        __syncthreads();
+    }
+
+    template <typename vmfDevType>
+    __global__ void ReLUDerivative(vmfDevType* x, vmfDevType y, cuSize arraySize)
+    {
+        cuSize threadIndexX = blockIdx.x * blockDim.x + threadIdx.x;
+        if (threadIndexX < arraySize) x[threadIndexX] = x[threadIndexX] > vmfDevType(0) ? y : vmfDevType(0.0);
         __syncthreads();
     }
 
@@ -24,12 +31,32 @@ namespace VMF_CUDA
         __syncthreads();
     }
 
+    template <typename vmfDevType>
+    __global__ void leakyReLUDerivative(vmfDevType* x, vmfDevType y, cuSize arraySize)
+    {
+        cuSize threadIndexX = blockIdx.x * blockDim.x + threadIdx.x;
+        if (threadIndexX < arraySize) x[threadIndexX] = x[threadIndexX] < vmfDevType(0) ? x[threadIndexX] / y : x[threadIndexX];
+        __syncthreads();
+    }
+
     // Sigmoid
     template <typename vmfDevType>
     __global__ void sigmoid(vmfDevType* x, cuSize arraySize)
     {
         cuSize threadIndexX = blockIdx.x * blockDim.x + threadIdx.x;
         if (threadIndexX < arraySize) x[threadIndexX] = vmfDevType(0.5) * (vmfDevType(1.0) + tanh(vmfDevType(0.5) * x[threadIndexX]));
+        __syncthreads();
+    }
+
+    template <typename vmfDevType>
+    __global__ void sigmoidDerivative(vmfDevType* x, cuSize arraySize)
+    {
+        cuSize threadIndexX = blockIdx.x * blockDim.x + threadIdx.x;
+        if (threadIndexX < arraySize)
+        {
+            const double sigmoidValue = 1 / (1 + exp(-x[threadIndexX]));
+            x[threadIndexX] = sigmoidValue * (1 - sigmoidValue);
+        }
         __syncthreads();
     }
 
